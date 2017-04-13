62c9940aa0c644ab839bca842681a5353baeca63
Matt Potter <mattpotter28@gmail.com>
2017-04-13 00:37:50 -0500

    testing out ideas in java

---
+++ src/nameScores.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Matt on 4/12/2017.
+ */
+public class nameScores {
+    public static void main(String[] args) {
+        name testName = new name("MATT");
+        System.out.println(testName);
+    }
+}
